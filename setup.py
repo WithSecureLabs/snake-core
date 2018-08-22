8f05d560620d396d9a06f41dc373200ac32b7fce
Alex Kornitzer <alex.kornitzer@countercept.com>
2018-08-22 21:09:11 +0100

    setup: bump pyyaml to support python 3.7

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         'marshmallow==3.0.0b7',
         'motor==1.2.1',
         'python-magic==0.4.15',
-        'pyyaml==3.12',
+        'pyyaml==3.13',
         'requests==2.18.4',
         'tornado==5.0.1',
         'webargs==2.0.0'
