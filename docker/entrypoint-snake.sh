c5585ee53563f405a1665d1cf7b3d8886ded13c6
Alex Kornitzer <alex.kornitzer@withsecure.com>
2023-06-29 11:14:46 +0100

    chore: update env vars for docker images

--- docker/entrypoint-snake.sh
+++ docker/entrypoint-snake.sh
@@ -7,14 +7,19 @@ export PYTHONPATH="/var/lib/snake/scales/lib/python`python3 -c "import sys; prin
 if [ "$1" = 'snake' ]; then
   sed -i 's/address: 127.0.0.1/address: 0.0.0.0/' /etc/snake/snake.conf
 
-  if [ $MONGODB_ADDRESS ] && [ $MONGODB_PORT ]; then
+  if [ $MONGODB_URI ]; then
+    sed -i "s/mongodb: \"mongodb:\/\/localhost:27017\"/mongodb: \"$MONGODB_URI\"/" /etc/snake/snake.conf
+  elif [ $MONGODB_ADDRESS ] && [ $MONGODB_PORT ]; then
     sed -i "s/mongodb: \"mongodb:\/\/localhost:27017\"/mongodb: \"mongodb:\/\/$MONGODB_ADDRESS:$MONGODB_PORT\"/" /etc/snake/snake.conf
   else
     echo "Please pass values for the MONGODB_ADDRESS and MONGODB_PORT"
     exit 1
   fi
 
-  if [ $REDIS_ADDRESS ] && [ $REDIS_PORT ]; then
+  if [ $REDIS_URI ]; then
+    sed -i "s/backend: 'redis:\/\/localhost:6379'/backend: '$REDIS_URI'/" /etc/snake/snake.conf
+    sed -i "s/broker: 'redis:\/\/localhost:6379\/0'/broker: '$REDIS_URI'/" /etc/snake/snake.conf
+  elif [ $REDIS_ADDRESS ] && [ $REDIS_PORT ]; then
     sed -i "s/backend: 'redis:\/\/localhost:6379'/backend: 'redis:\/\/$REDIS_ADDRESS:$REDIS_PORT'/" /etc/snake/snake.conf
     sed -i "s/broker: 'redis:\/\/localhost:6379\/0'/broker: 'redis:\/\/$REDIS_ADDRESS:$REDIS_PORT\/0'/" /etc/snake/snake.conf
   else
@@ -31,6 +36,15 @@ if [ "$1" = 'snake' ]; then
 
   if [ $SNAKE_COMMAND_AUTORUNS ]; then
     sed -i "s/command_autoruns: True/command_autoruns: $SNAKE_COMMAND_AUTORUNS/" /etc/snake/snake.conf
+  fi
+
+  if [ $SNAKE_OPENID_URI ]; then
+    echo "authentication:\n  scheme: 'oauth'\n  openid_url: '$SNAKE_OPENID_URI'\n" >> /etc/snake/snake.conf
+  fi
+
+  if [ $SNAKE_S3_BUCKET_NAME ]; then
+    sed -i "s/file_db: '/var/db/snake'/file_db: $SNAKE_S3_BUCKET_NAME/" /etc/snake/snake.conf
+    sed -i "s/storage: 'file'/storage: 's3'/" /etc/snake/snake.conf
   fi
 
   if [ $SNAKE_SCALES_DIR ]; then
